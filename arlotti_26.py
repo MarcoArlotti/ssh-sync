628b4c8a502758d60023b3aa9ddc28989bc0f544
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2024-02-06 11:19:39 +0100

    es 26 fixed

--- arlotti_26.py
+++ arlotti_26.py
@@ -7,23 +7,23 @@ c = float(input("mettere la c\n"))
 def delta(a: float,b: float,c: float) -> list[float] | None:
     delta = b*b-4*a*c
     if delta < 0:
-        return [None,None]
+        risX1 = None
+        risX2 = None
     elif delta == 0:
         risX1 = -b / (2*a)
         risX2 = -b / (2*a)
     else:
         risX1 = (-b + sqrt(delta) / (2 *a))
         risX2 = (-b - sqrt(delta) / (2 *a))
-    return [risX1,risX2]
+    return risX1,risX2
 
-def print_ris(soluz: list[float] | None) -> None:
+def print_ris(risX1,risX2) -> None:
     
-    if lista_soluzioni == None:
+    if  risX1== None and risX2 == None:
         print("il delta è negativo, perciò ERROR")   
 
     else:
-        risX1, risX2  = soluz 
         print(f"x1 corrisponde a {risX1}\nx2 corisponde a {risX2}")    
 
-lista_soluzioni = delta(a,b,c)
-print_ris(soluz)
+risX1,risX2 = delta(a,b,c)
+print_ris(risX1,risX2)
