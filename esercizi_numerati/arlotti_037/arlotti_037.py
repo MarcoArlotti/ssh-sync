e5baa9ad58683aa26a51917dfac2e50fdf8fa53d
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2024-04-17 10:51:25 +0200

    fix

---
+++ esercizi_numerati/arlotti_037/arlotti_037.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+import json
+app = Flask(__name__)
+
+@app.route("/")
+def mostra_link():
+    with open("esercizi_numerati/arlotti_037/arlotti_037.json","r") as lista_link:
+        lista_link = json.load(lista_link)
+        return render_template("esercizi_numerati/arlotti_037/templates/arlotti_037.html",lista_link=lista_link)
+    
