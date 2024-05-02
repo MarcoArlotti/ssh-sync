61d000ff7a7017470ac4c6458735b120a1d76065
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2024-05-02 13:47:43 +0200

    fix

--- esercizi_numerati/arlotti_039/arlotti_039.py
+++ esercizi_numerati/arlotti_039/arlotti_039.py
@@ -4,26 +4,47 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-with open("esercizi_numerati/arlotti_039/arlotti_039.json") as lista_quesiti:
-    lista_quesiti = json.load(lista_quesiti)
+def read_flashcards(filename: str) -> None:
+    with open(filename, "r") as file_json:
+        lista_domande = json.load(file_json)
+        return lista_domande
+    
+def get_flashcard_by_id(id: int,lista_domande) -> None:
+    for domanda in lista_domande:
+        id_ = domanda["id"]
+        if id_ == id:
+            domanda = domanda["question"]
+            print(domanda)
 
-@app.route("/", methods=["GET", "POST"])
-def lista_quesiti(id):
-    lista_quesiti = None
-    for quesito in lista_quesiti:
-        if quesito["id"] == id:
-    if lista_quesiti == None:
-        return "domanda non trovata \nsuggerimento: (mettere un numero/numero troppo alto)"
+def prompt_for_id() -> int:
+    rifai = True
+    while rifai == True:
+        rifai = False
+        id_scelto = input("inserire l'id della domanda che si vuole visulizzare")
+        try:
+            id_scelto = int(id_scelto)
+        except:
+            rifai = True
+    return id_scelto
 
-    if request.method == "POST":
-        
-        with open("esercizi_numerati/arlotti_039/arlotti_039.json") as lista_quesiti:
-            lista_quesiti = json.load(lista_quesiti)
+def get_flashcard_by_id(id_scelto):
 
-            risposta_data = request.form.get("risposta_data")
-        return 
+    for domanda in lista_domande:
+        id = domanda["id"]
+        if id_scelto == id:
+            print(domanda["question"])
+            break
+    return lista_domande
+
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5069)
+    #app.run(debug=True,port=5069)
+    filename = "esercizi_numerati/arlotti_039/arlotti_039.json"
+    lista_domande = read_flashcards(filename)
+    id = 2
+    get_flashcard_by_id(id,lista_domande)
+    id_scelto = prompt_for_id()
+    get_flashcard_by_id(id_scelto)
+    print(lista_domande)
