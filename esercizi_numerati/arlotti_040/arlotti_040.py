620e988672657b7593d51001ad09b103974527c9
MarcoArlotti <marco.arlotti@studenti.isissgobetti.it>
2024-05-08 18:53:08 +0200

    fix

--- esercizi_numerati/arlotti_040/arlotti_040.py
+++ esercizi_numerati/arlotti_040/arlotti_040.py
@@ -1,18 +1,28 @@
 import json
 from flask import Flask, render_template
+filename = "esercizi_numerati/arlotti_040/arlotti_040.json"
 app = Flask(__name__)
 @app.route("/")
 def link():
-    return render_template('arlotti_040.html', recipe=recipe)
+    return render_template('arlotti_040.html', link=link)
+
+@app.route('/recipes')
+def recipes():
+    filename = "esercizi_numerati/arlotti_040/arlotti_040.json"
+    ricette = read_recipes(filename)
+    return render_template("pagina_ricette.html", ricette=ricette)
 
 @app.route('/recipe/<int:id>')
 def recipe(id):
     recipe = get_recipe_by_id(id)
-    return render_template('arlotti_040.html', recipe=recipe)
+    return render_template('recipe.html', recipe=recipe)
+
+
 
 def read_recipes(filename: str) -> None:
     with open(filename,"r") as file_json:
         ricette = json.load(file_json)
+    return ricette
 
 def prompt_for_id() -> int:
     id = input("inserire un id")
@@ -28,9 +38,9 @@ def prompt_for_id() -> int:
 
 def get_recipe_by_id(id: int) -> None:
     with open(filename,"r") as file_json:
-        ricette = json.load(file_json)
+        recipes = json.load(file_json)
 
-    for ricetta in ricette:
+    for ricetta in recipes:
         id_ricetta = ricetta["id"]
         if id_ricetta == id:
             print(ricetta)
@@ -40,7 +50,6 @@ def get_recipe_by_id(id: int) -> None:
 
 
 
-filename = "esercizi_numerati/arlotti_040/arlotti_040.json"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5069)
--- miscellaneous/coin_flip.py
+++ miscellaneous/coin_flip.py
@@ -4,7 +4,7 @@ import random
 
 flip = ["you are going first","you are going second"]
 while True:
-    print(random.choices(flip,weights=[0.1,99]))
+    print(random.choices(flip,weights=[50,50]))
 #print("-"*50)
 #print(f"è uscito {print(random.choices(flip))}")
 #print("-"*50)
